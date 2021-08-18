239165445f666cce3da99ae0c884411a808718a4
ANrajin <dev.quadque@gmail.com>
2021-08-18 11:56:24 +0600

    role, permission, user table modified

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function isAdmin()
+    {
+        return in_array($this->email, config('learnque.admins'));
+    }
 }
--- config/learnque.php
+++ config/learnque.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'admin' => [
+    'admins' => [
         'admin@leanque.com'
     ]
 ];
