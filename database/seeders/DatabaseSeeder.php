82126159e81d8d818f97ad8d2d8d589f91f55b64
ANrajin <dev.quadque@gmail.com>
2021-10-14 17:30:29 +0600

    admin db configured

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         User::create([
             'name' => 'admin',
-            'email' => 'admin@learnque.com',
+            'email' => 'admin@quadque.tech',
             'password' => Hash::make('admin')
         ]);
 
